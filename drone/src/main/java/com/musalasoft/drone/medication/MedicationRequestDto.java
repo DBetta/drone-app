c4f883a83db3c7868aec856cd66bcc44ff887cb1
Denis Gitonga <denisbgitonga@gmail.com>
2022-02-03 23:46:09 +0300

    add medication validation

--- drone/src/main/java/com/musalasoft/drone/medication/MedicationRequestDto.java
+++ drone/src/main/java/com/musalasoft/drone/medication/MedicationRequestDto.java
@@ -5,6 +5,11 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 /**
  * @author Denis Gitonga
  */
@@ -13,10 +18,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MedicationRequestDto {
+    @NotBlank(message = "name cannot be null/empty")
+    @Pattern(regexp = "^[a-zA-Z0-9_\\-]+$", message = "Name can only contain letters, numbers, '-', '_'")
     private String name;
 
+    @Min(0)
+    @Max(500)
     private int weight;
 
+
+    @NotBlank(message = "code cannot be null/empty")
+    @Pattern(regexp = "^[A-Z0-9_]+$", message = "Name can only contain letters, numbers, '-', '_'")
     private String code;
 
     private String image;
